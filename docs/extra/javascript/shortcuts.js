90ce661cdf67f92b50644e56f8eabec73751d4a6
Xiaokang2022 <2951256653@qq.com>
2024-08-19 07:05:42 +0800

    Updated docs

--- docs/extra/javascript/shortcuts.js
+++ docs/extra/javascript/shortcuts.js
@@ -1,19 +1,21 @@
-keyboard$.subscribe(function (key) {  // 键盘左键上一页
-    if (key.mode === "global" && key.type === "ArrowLeft") {
-        /* Add custom keyboard handler here */
-        var elements = document.getElementsByClassName('md-footer__link md-footer__link--prev');
-        if (elements.length)
-            elements[0].click();
-        key.claim()
-    }
-})
+// 键盘左键上一页
+keyboard$.subscribe(function (key) {
+  if (key.mode === "global" && key.type === "ArrowLeft") {
+    var elements = document.getElementsByClassName(
+      "md-footer__link md-footer__link--prev"
+    );
+    if (elements.length) elements[0].click();
+    key.claim();
+  }
+});
 
-keyboard$.subscribe(function (key) {  // 键盘右键下一页
-    if (key.mode === "global" && key.type === "ArrowRight") {
-        /* Add custom keyboard handler here */
-        var elements = document.getElementsByClassName('md-footer__link md-footer__link--next');
-        if (elements.length)
-            elements[0].click();
-        key.claim()
-    }
-})
+// 键盘右键下一页
+keyboard$.subscribe(function (key) {
+  if (key.mode === "global" && key.type === "ArrowRight") {
+    var elements = document.getElementsByClassName(
+      "md-footer__link md-footer__link--next"
+    );
+    if (elements.length) elements[0].click();
+    key.claim();
+  }
+});
