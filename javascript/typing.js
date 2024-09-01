95dae1e40e0dc4a321c04f30dee93017dfd7f292
Xiaokang2022 <2951256653@qq.com>
2024-09-02 01:09:10 +0800

    Deployed a0e98ab with MkDocs version: 1.6.0

--- javascript/typing.js
+++ javascript/typing.js
@@ -2,9 +2,17 @@ const txt = [
   "欢迎来到 tkintertools 的官方网站，希望本网站能对您有一定的帮助！",
 ];
 
+const txt_en = [
+  "Welcome to the official website of tkintertools, I hope this website can be of some help to you!",
+];
+
 var index = 0;
 var i = 0;
 var switch_flag = true;
+
+var index_en = 0;
+var i_en = 0;
+var switch_flag_en = true;
 
 setInterval(function () {
   if (switch_flag) {
@@ -22,6 +30,32 @@ setInterval(function () {
     i++;
     if (i >= txt.length) {
       i = 0;
+    }
+  }
+}, 40); /*50间隔时间*/
+
+setInterval(function () {
+  if (switch_flag_en) {
+    document.querySelector(".text_en").innerHTML = txt_en[i_en].slice(
+      0,
+      ++index_en
+    );
+  } else {
+    document.querySelector(".text_en").innerHTML = txt_en[i_en].slice(
+      0,
+      index_en--
+    );
+  }
+
+  if (index_en == txt_en[i_en].length + 25) {
+    /*25*40 停顿时间*/
+    switch_flag_en = false;
+  } else if (index_en == 0) {
+    index_en = 0;
+    switch_flag_en = true;
+    i_en++;
+    if (i_en >= txt_en.length) {
+      i_en = 0;
     }
   }
 }, 40); /*50间隔时间*/
