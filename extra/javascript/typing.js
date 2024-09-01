c6a144e1ea5cdea6ca95c5134041303ac00fa8ee
Xiaokang2022 <2951256653@qq.com>
2024-09-01 23:34:27 +0800

    Deployed b415f3b with MkDocs version: 1.6.0

--- extra/javascript/typing.js
+++ extra/javascript/typing.js
@@ -1,34 +1,27 @@
 const txt = [
-    "欢迎来到 tkintertools 的官方网站，希望本网站能对您有一定的帮助！",
-    "Welcome to the official website of tkintertools, I hope this website can be of some help to you!"
+  "欢迎来到 tkintertools 的官方网站，希望本网站能对您有一定的帮助！",
 ];
 
 var index = 0;
 var i = 0;
 var switch_flag = true;
 
-setInterval(function(){
-    
-    if(switch_flag){      
-        document.querySelector('.text').innerHTML = txt[i].slice(0,++index);
-    }
-    else{
-        document.querySelector('.text').innerHTML = txt[i].slice(0,index--);
-    }
+setInterval(function () {
+  if (switch_flag) {
+    document.querySelector(".text").innerHTML = txt[i].slice(0, ++index);
+  } else {
+    document.querySelector(".text").innerHTML = txt[i].slice(0, index--);
+  }
 
-    if(index==txt[i].length+25) /*25*40 停顿时间*/
-    {
-        switch_flag = false;
+  if (index == txt[i].length + 25) {
+    /*25*40 停顿时间*/
+    switch_flag = false;
+  } else if (index == 0) {
+    index = 0;
+    switch_flag = true;
+    i++;
+    if (i >= txt.length) {
+      i = 0;
     }
-    else if(index==0)
-    {
-        index = 0;
-        switch_flag = true;
-        i++;
-        if(i>=txt.length)
-        {
-            i=0; 
-        }
-    }
-
-}, 40) /*50间隔时间*/
+  }
+}, 40); /*50间隔时间*/
