aca764d785837ba1037f0fc297b04bce8f4a6927
小康2022 <2951256653@qq.com>
2024-11-05 11:22:02 +0800

    Deployed 23a985a with MkDocs version: 1.6.1

--- en/js/tablesort.js
+++
@@ -1,8 +0,0 @@
-// https://squidfunk.github.io/mkdocs-material/reference/data-tables/
-
-document$.subscribe(function () {
-  var tables = document.querySelectorAll("article table:not([class])");
-  tables.forEach(function (table) {
-    new Tablesort(table);
-  });
-});
